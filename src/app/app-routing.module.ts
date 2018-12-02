dde33b7a8bc5e54191d2db12440554b2501550c6
Marcus Adrian <marcus.adrian75@gmail.com>
2018-12-02 10:44:55 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {OwnerSearchComponent} from './owner/owner-search/owner-search.component';
+import {OwnerDetailComponent} from './owner/owner-detail/owner-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: 'owners', children: [
+      {path: 'search', component: OwnerSearchComponent},
+      {path: ':id', component: OwnerDetailComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
+import { HomeComponent } from './home/home.component';
+import { OwnerDetailComponent } from './owner/owner-detail/owner-detail.component';
+import { OwnerSearchComponent } from './owner/owner-search/owner-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OwnerDetailComponent,
+    OwnerSearchComponent
   ],
   imports: [
     BrowserModule,
